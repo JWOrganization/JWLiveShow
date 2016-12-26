f6024b7245f31a962a725582a0205c37b6de2fd7
1457792186 <1457792186@qq.com>
2016-12-26 18:52:40 +0800

    JW:Create Base Project

---
+++ MDLiveShow/MDLiveShowTests/MDLiveShowTests.m
@@ -0,0 +1,39 @@
+//
+//  MDLiveShowTests.m
+//  MDLiveShowTests
+//
+//  Created by TianWei You on 16/12/26.
+//  Copyright © 2016年 TianWei You. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MDLiveShowTests : XCTestCase
+
+@end
+
+@implementation MDLiveShowTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
